b33c09764d02886fe85339f685d73549fb6e4f97
“vinay-vrp” <vinayrpatil12@gmail.com>
2024-10-15 12:51:01 -0700

    first-commit

---
+++ dict.py
@@ -0,0 +1,12 @@
+info = {
+    "name" : "vinay",
+    "age" : 45,
+    "sub" :["math","science","bio","mf"],
+    id : 12
+}
+print(info)
+
+info["name"]= "jhon-wick"
+print(info)
+info["shambhu"] = 456
+print(info)
