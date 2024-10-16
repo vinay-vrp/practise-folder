b0c9ec0add2f0c9a6a0d39af0f7e2359d202da97
“vinay-vrp” <vinayrpatil12@gmail.com>
2024-10-15 21:26:37 -0700

    second-commit

--- dict.py
+++ dict.py
@@ -1,12 +1,30 @@
-info = {
-    "name" : "vinay",
-    "age" : 45,
-    "sub" :["math","science","bio","mf"],
-    id : 12
-}
-print(info)
+# info = {
+#     "name" : "vinay",
+#     "age" : 45,
+#     "sub" :["math","science","bio","mf"],
+#     id : 12
+# }
+# print(info)
 
-info["name"]= "jhon-wick"
-print(info)
-info["shambhu"] = 456
-print(info)
+# info["name"]= "jhon-wick"
+# print(info)
+# info["shambhu"] = 456
+# print(info)
+
+name ={
+    "name ": "vinay",
+      "sub" :{
+          "maht":15,
+          "scie" :45,
+          "bio" : 78 ,        
+
+    }
+}
+
+print(name.keys())
+print(list(name.values()))
+print(name.items())
+print(name.get('name'))
+print(name.update({"city":"delhi"}))
+name.update({"city":"delhi"})
+print(name )
