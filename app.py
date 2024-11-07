e4390708f0950688f61563f0cfa8d0e857ac0f49
“vinay-vrp” <vinayrpatil12@gmail.com>
2024-11-07 13:28:32 +0530

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-a = 25
-b = 26
+# a = 25
+# b = 26
+# 
+# sum = a+b
+# print("total no =",sum)
 
-sum = a+b
-print("total no =",sum)
+
+a=int(input("enter the first no="))
+b=int(input("enter the second no="))
+print(a+b)
